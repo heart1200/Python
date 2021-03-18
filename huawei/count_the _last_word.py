f662fd83a55a126ef3c61cc04589467264924c9b
Luodayu <heart1200@github.com>
2021-03-18 11:33:51 +0800

    update codes

--- huawei/count_the _last_word.py
+++ huawei/count_the _last_word.py
@@ -1,6 +1,7 @@
 # 题目：计算字符串最后一个单词的长度，单词以空格隔开。
 # 输入一行，代表要计算的字符串，非空，长度小于5000。
 
+
 # 使用自带函数解决
 def count_the_last_word_in_func(string):
     if 0 < len(string) < 5000:
@@ -10,7 +11,7 @@ def count_the_last_word_in_func(string):
         print("too much char or null")
 
 
-# 使用常规方法
+# 使用for循环
 def count_the_last_word_in_normal(string):
     length = 0
     for i in range(len(string), -1, -1):
